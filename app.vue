dc96c8b7653790333702a85d6c22ae5c502013be
xanzhu <34955679+xanzhu@users.noreply.github.com>
2022-07-28 19:00:17 +1200

    test 2

--- app.vue
+++ app.vue
@@ -1,5 +1,9 @@
 <template>
   <div>
-      <nuxt-img src="/city.jpeg" height="600" provider="vercel"/>
+    <nuxt-img src="/city.jpeg" height="600" />
+    <nuxt-img src="city.jpeg" height="600" />
+    <nuxt-img src="/city.jpeg" />
+    <nuxt-picture src="/city.jpeg" />
+    <nuxt-picture src="/city.jpeg" format="webp" />
   </div>
 </template>
